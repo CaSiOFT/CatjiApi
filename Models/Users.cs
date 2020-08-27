39700034689ea365ccc922b258a1e6be5ba929bb
Frezcirno <2277861660@qq.com>
2020-08-27 12:14:49 +0800

    小修小补

--- Models/Users.cs
+++ Models/Users.cs
@@ -3,32 +3,8 @@ using System.Collections.Generic;
 
 namespace CatjiApi.Models
 {
-    public partial class Users
+    public class Users
     {
-        public Users()
-        {
-            BlockBlockUs = new HashSet<Block>();
-            BlockUs = new HashSet<Block>();
-            Blog = new HashSet<Blog>();
-            Blogcomment = new HashSet<Blogcomment>();
-            Cat = new HashSet<Cat>();
-            Favorite = new HashSet<Favorite>();
-            FollowFollowUs = new HashSet<Follow>();
-            FollowUs = new HashSet<Follow>();
-            Likeblog = new HashSet<Likeblog>();
-            Likeblogcomment = new HashSet<Likeblogcomment>();
-            Likevideo = new HashSet<Likevideo>();
-            Likevideocomment = new HashSet<Likevideocomment>();
-            MessageToUs = new HashSet<Message>();
-            MessageUs = new HashSet<Message>();
-            Reportblog = new HashSet<Reportblog>();
-            Reportvideo = new HashSet<Reportvideo>();
-            Searchhistory = new HashSet<Searchhistory>();
-            Video = new HashSet<Video>();
-            Videocomment = new HashSet<Videocomment>();
-            Watchhistory = new HashSet<Watchhistory>();
-        }
-
         public int Usid { get; set; }
         public string Email { get; set; }
         public string Tel { get; set; }
@@ -45,25 +21,25 @@ namespace CatjiApi.Models
         public DateTime ChangedTime { get; set; }
 
         public Cat CatNavigation { get; set; }
-        public ICollection<Block> BlockBlockUs { get; set; }
-        public ICollection<Block> BlockUs { get; set; }
-        public ICollection<Blog> Blog { get; set; }
-        public ICollection<Blogcomment> Blogcomment { get; set; }
-        public ICollection<Cat> Cat { get; set; }
-        public ICollection<Favorite> Favorite { get; set; }
-        public ICollection<Follow> FollowFollowUs { get; set; }
-        public ICollection<Follow> FollowUs { get; set; }
-        public ICollection<Likeblog> Likeblog { get; set; }
-        public ICollection<Likeblogcomment> Likeblogcomment { get; set; }
-        public ICollection<Likevideo> Likevideo { get; set; }
-        public ICollection<Likevideocomment> Likevideocomment { get; set; }
-        public ICollection<Message> MessageToUs { get; set; }
-        public ICollection<Message> MessageUs { get; set; }
-        public ICollection<Reportblog> Reportblog { get; set; }
-        public ICollection<Reportvideo> Reportvideo { get; set; }
-        public ICollection<Searchhistory> Searchhistory { get; set; }
-        public ICollection<Video> Video { get; set; }
-        public ICollection<Videocomment> Videocomment { get; set; }
-        public ICollection<Watchhistory> Watchhistory { get; set; }
+        public ICollection<Block> BlockBlockUs { get; set; } = new HashSet<Block>();
+        public ICollection<Block> BlockUs { get; set; } = new HashSet<Block>();
+        public ICollection<Blog> Blog { get; set; } = new HashSet<Blog>();
+        public ICollection<Blogcomment> Blogcomment { get; set; } = new HashSet<Blogcomment>();
+        public ICollection<Cat> Cat { get; set; } = new HashSet<Cat>();
+        public ICollection<Favorite> Favorite { get; set; } = new HashSet<Favorite>();
+        public ICollection<Follow> FollowFollowUs { get; set; } = new HashSet<Follow>();
+        public ICollection<Follow> FollowUs { get; set; } = new HashSet<Follow>();
+        public ICollection<Likeblog> Likeblog { get; set; } = new HashSet<Likeblog>();
+        public ICollection<Likeblogcomment> Likeblogcomment { get; set; } = new HashSet<Likeblogcomment>();
+        public ICollection<Likevideo> Likevideo { get; set; } = new HashSet<Likevideo>();
+        public ICollection<Likevideocomment> Likevideocomment { get; set; } = new HashSet<Likevideocomment>();
+        public ICollection<Message> MessageToUs { get; set; } = new HashSet<Message>();
+        public ICollection<Message> MessageUs { get; set; } = new HashSet<Message>();
+        public ICollection<Reportblog> Reportblog { get; set; } = new HashSet<Reportblog>();
+        public ICollection<Reportvideo> Reportvideo { get; set; } = new HashSet<Reportvideo>();
+        public ICollection<Searchhistory> Searchhistory { get; set; } = new HashSet<Searchhistory>();
+        public ICollection<Video> Video { get; set; } = new HashSet<Video>();
+        public ICollection<Videocomment> Videocomment { get; set; } = new HashSet<Videocomment>();
+        public ICollection<Watchhistory> Watchhistory { get; set; } = new HashSet<Watchhistory>();
     }
 }
